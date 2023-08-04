8880952d436c10043a2d4589d084151785bfbe16
Muhammadali Amonov <amonovmuhammadali04@gmail.com>
2023-08-04 22:16:22 +0500

    birinchi urinish

---
+++ github/index.js
@@ -0,0 +1,14 @@
+// git init
+// git remote add origin URL
+
+
+
+// git add .
+// git add /test.js
+// git commit -m " "
+// git push origin master 
+
+// git branch
+// git checkout branchname
+
+// git pull
